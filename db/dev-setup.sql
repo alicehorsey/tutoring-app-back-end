1c2e559ecd554ecbf4e2ed7d7c12adceddb6875c
Alice Horsey <alicehorsey@gmail.com>
2021-02-03 14:50:08 +0000

    db set up creates all required tables

--- db/dev-setup.sql
+++ db/dev-setup.sql
@@ -11,4 +11,54 @@ CREATE TABLE parents (
     phone_number INT NOT NULL,
     email_address VARCHAR(255) NOT NULL,
     additional_information VARCHAR(255)
-)
+);
+
+CREATE TABLE pupils (
+    pupil_id SERIAL PRIMARY KEY,
+    parent_id INT REFERENCES parents(parent_id) NOT NULL,
+    pupil_forename VARCHAR(255) NOT NULL,
+    pupil_surname VARCHAR(255) NOT NULL,
+    starting_date DATE,
+    dob DATE,
+    school VARCHAR(255) NOT NULL,
+    year_group INT NOT NULL,
+    handedness VARCHAR(255) NOT NULL,
+    prelim_information VARCHAR(255),
+    pupil_information VARCHAR(255)
+);
+
+CREATE TABLE topics (
+    topic_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE reading_books (
+    reading_book_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    publisher VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE work_books (
+    work_book_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    publisher VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE topics_junction (
+    pupil_id INT REFERENCES pupils(pupil_id) NOT NULL, 
+    topic_id INT REFERENCES topics(topic_id) NOT NULL,
+    completed BOOLEAN
+);
+
+CREATE TABLE reading_books_junction (
+    pupil_id INT REFERENCES pupils(pupil_id) NOT NULL, 
+    reading_book_id INT REFERENCES reading_books(reading_book_id) NOT NULL,
+    completed BOOLEAN
+);
+
+CREATE TABLE work_books_junction (
+    pupil_id INT REFERENCES pupils(pupil_id) NOT NULL, 
+    work_book_id INT REFERENCES work_books(work_book_id) NOT NULL,
+    completed BOOLEAN
+);
