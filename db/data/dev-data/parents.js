4e61a7da47ff1804c56881fe3716ec050489dbeb
Alice Horsey <alicehorsey@gmail.com>
2021-02-03 16:22:37 +0000

    changes phone num data type to string

--- db/data/dev-data/parents.js
+++ db/data/dev-data/parents.js
@@ -3,7 +3,7 @@ module.exports = [
         parent_forename: 'Mandy',
         parent_surname: 'Brown',
         home_address: 'The House, The Grove',
-        phone_number: 00000111222,
+        phone_number: '00000111222',
         email_address: 'mandy@mandybrown.com',
         additional_information: 'contact best via text as works shifts'
     },
@@ -11,7 +11,7 @@ module.exports = [
         parent_forename: 'David',
         parent_surname: 'Smith',
         home_address: 'The Big House, Town Square',
-        phone_number: 00000123456,
+        phone_number: '00000123456',
         email_address: 'david@davidsmith.co.uk',
         additional_information: 'works 9-5'
     },
@@ -19,7 +19,7 @@ module.exports = [
         parent_forename: 'Sarah',
         parent_surname: 'Williams',
         home_address: 'The Bungalow, Little Town',
-        phone_number: 00000000111,
+        phone_number: '00000000111',
         email_address: 'sarah@williams.com',
         additional_information: 'works 9-5'
     },
@@ -27,7 +27,7 @@ module.exports = [
         parent_forename: 'Julie',
         parent_surname: 'Thompson',
         home_address: '10 The Lane, Little Town',
-        phone_number: 00000222333,
+        phone_number: '00000222333',
         email_address: 'julie@juliethompson.com',
         additional_information: 'internet quality sometimes poor'
     },
@@ -35,7 +35,7 @@ module.exports = [
         parent_forename: 'Amanda',
         parent_surname: 'Wilson',
         home_address: '45 The Road, Grove Town',
-        phone_number: 00000444555,
+        phone_number: '00000444555',
         email_address: 'amanda@amandawilson.com',
         additional_information: 'younger sibling interested in tutoring from 2023'
     }
