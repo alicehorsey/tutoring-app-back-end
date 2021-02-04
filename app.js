112cb2157d5a3c24cf9141f9e633c1c57603c555
Alice Horsey <alicehorsey@gmail.com>
2021-02-04 22:35:11 +0000

    sets up app to listen on 8080 for local host

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const apiRouter = require("./routes/apiRouter");
 app.use(express.json());
+const port = 8080;
 
 app.use("/api", apiRouter);
 
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
 
 
 
